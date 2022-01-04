ff7167950b7f70e3b482e49f8331ff4ce13b9ed3
phuongnguyen2106 <phuong.nguyen@wizeline.com>
2022-01-05 00:14:42 +0700

    remove log

--- part-3/frontend/src/app/core/services/visitor.service.ts
+++ part-3/frontend/src/app/core/services/visitor.service.ts
@@ -12,20 +12,14 @@ export class VisitorService {
   constructor(private httpClient: HttpClient) { }
 
   GetCurrentVisitor(): Observable<GetOneVisitorResponse> {
-    console.log(this.apiEndpoint+`/current`);
-    
     return this.httpClient.get<GetOneVisitorResponse>(this.apiEndpoint+`/current`);
   }
 
   GetLast100Visitors(): Observable<Get100VisitorsResponse> {
-    console.log(this.apiEndpoint+`/last`);
-
     return this.httpClient.get<Get100VisitorsResponse>(this.apiEndpoint+`/last`);
   }
 
   GetTop100Visitors(): Observable<Get100VisitorsResponse> {
-    console.log(this.apiEndpoint + `/top`);
-    
     return this.httpClient.get<Get100VisitorsResponse>(this.apiEndpoint+`/top`);
   }
 }
